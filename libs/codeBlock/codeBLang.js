e14c0586fd5ac3f207e456b415214cdeaf644ad4
hikair <351477856@qq.com>
2020-11-11 17:24:13 +0800

    Site updated: 2020-11-11 17:24:13

--- libs/codeBlock/codeBLang.js
+++ libs/codeBlock/codeBLang.js
@@ -3,7 +3,7 @@
 $(function () {
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
 
-  $('pre').before($highlight_lang);
+  $('pre').after($highlight_lang);
   $('pre').each(function () {
     var code_language = $(this).attr('class');
 
@@ -11,7 +11,7 @@ $(function () {
       return true;
     };
     var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
-
+    
     // 首字母大写
     // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
     
